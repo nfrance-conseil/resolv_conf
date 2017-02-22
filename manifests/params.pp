31d93d38c2a86703d19b531912ff0f524325dc4d
Geoff Williams <geoff.williams@puppetlabs.com>
2017-02-22 11:23:09 +1100

    use gid=0 for cross-platform support, release

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,21 +7,19 @@ class resolv_conf::params {
   $resolv_conf_owner  = "root"
   $resolv_conf_mode   = "0644"
   $search             = ""
+  $resolv_conf_group  = "0"
 
   case $facts['os']['family'] {
     "AIX": {
-      $resolv_conf_group  = "root"
       $search_keyword     = "domain"
       $options            = ["rotate", "timeout:2", "attempts:2"]
     }
     "Solaris": {
-      $resolv_conf_group  = "sys"
       $search_keyword     = "search"
       $options            = []
     }
     "RedHat": {
       $search_keyword     = "search"
-      $resolv_conf_group  = "root"
       $options            = []
     }
     default: {
