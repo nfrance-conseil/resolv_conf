cd6215fe0b72645bfffc81d12efb82d782007208
Joris Dedieu <joris.dedieu@nfrance.com>
2017-03-10 15:19:52 +0100

    Change some quote stuff to pass puppetlint and add FreeBSD support

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,24 +2,28 @@
 #
 # Params pattern
 class resolv_conf::params {
-  $resolv_conf_path   = "/etc/resolv.conf"
-  $nameservers        = ["8.8.8.8"]
-  $resolv_conf_owner  = "root"
-  $resolv_conf_mode   = "0644"
-  $search             = ""
-  $resolv_conf_group  = "0"
+  $resolv_conf_path   = '/etc/resolv.conf'
+  $nameservers        = ['8.8.8.8']
+  $resolv_conf_owner  = 'root'
+  $resolv_conf_mode   = '0644'
+  $search             = ''
+  $resolv_conf_group  = '0'
 
   case $facts['os']['family'] {
-    "AIX": {
-      $search_keyword     = "domain"
-      $options            = ["rotate", "timeout:2", "attempts:2"]
+    'AIX': {
+      $search_keyword     = 'domain'
+      $options            = ['rotate', 'timeout:2', 'attempts:2']
     }
-    "Solaris": {
-      $search_keyword     = "search"
+    'Solaris': {
+      $search_keyword     = 'search'
       $options            = []
     }
-    "RedHat": {
-      $search_keyword     = "search"
+    'RedHat': {
+      $search_keyword     = 'search'
+      $options            = []
+    }
+    'FreeBSD': {
+      $search_keyword     = 'search'
       $options            = []
     }
     default: {
